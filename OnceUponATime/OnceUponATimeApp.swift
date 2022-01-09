8a9e48066c6bbd5e2256de374dad9d9e7e795653
Spencer Belton <23slowmojo@gmail.com>
2022-01-08 23:56:01 -0700

    Initial Commit, Blank Project

---
+++ OnceUponATime/OnceUponATimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnceUponATimeApp.swift
+//  OnceUponATime
+//
+//  Created by Spencer Belton on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct OnceUponATimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
